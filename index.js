7b68441f77c63363e4757daa8578ee08a2a04c52
loadcode229 <sepone229@gmail.com>
2021-03-30 21:17:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, string) {
+    const newArr = [];
+    for (let i = 0; i < names.length; i++ ) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${string} gift!`)
+    }
+    return newArr;
+}
+
+function countDown(posInt) {
+    while (posInt >= 0) {
+        console.log(posInt--)
+    }
+}
